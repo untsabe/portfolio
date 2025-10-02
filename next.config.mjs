60ec3881458d874911c43f907cda9e8ff9b7d8dd
umenelisi shadrack ntsabe <163734026+untsabe@users.noreply.github.com>
2025-10-02 23:05:20 +0200

    fix: Add image configuration to fix loading issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  images: {
+    unoptimized: true, // This will fix image loading on Vercel
+  },
 };
 
 export default nextConfig;
